61f4dde7d7f366a8b632ecf57b67f378c7df2fce
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2020-10-31 15:47:28 +0530

    Init spring boot project

---
+++ src/main/java/lk/ijse/ws/stater/StaterApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.stater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/ws/stater/StaterApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.stater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
