3e08702eb648be3222a4a1bddede419561e224c6
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2020-11-07 14:38:39 +0530

    connect JDBC

--- src/main/java/lk/ijse/ws/stater/api/MyRestController.java
+++ src/main/java/lk/ijse/ws/stater/api/MyRestController.java
@@ -3,10 +3,9 @@ package lk.ijse.ws.stater.api;
 import lk.ijse.ws.stater.data.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
 import java.util.Date;
 
 
@@ -22,5 +21,24 @@ public class  MyRestController {
         msg.setMessage("Hello World");
         msg.setSentTime((new Date()));
         return msg;
+    }
+
+    @GetMapping(path = "/api/message")
+   public Message messageBuilder(
+           @RequestParam("username")
+           Spring user,
+           @RequestParam("message")
+           String message){
+        Message msg = new Message();
+        msg.setSender("Server");
+        msg.setMessage("Hello World");
+        msg.setSentTime((new Date()));
+        return msg;
+    }
+
+    @RequestMapping(value = "/api/message", method = RequestMethod.POST)
+    public Message createMessage(@RequestBody() Message message) {
+        System.out.println(message);
+        return message;
     }
 }
